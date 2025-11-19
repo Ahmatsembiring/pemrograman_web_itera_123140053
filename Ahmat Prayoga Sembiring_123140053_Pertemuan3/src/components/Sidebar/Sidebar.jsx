bc9b4f68fff5406eeed045feac32f174ec72227c
Ahmat Prayoga Sembiring <ahmadprayoga099@gmail.com>
2025-11-19 16:29:03 +0700

    update fitur

--- Ahmat Prayoga Sembiring_123140053_Pertemuan3/src/components/Sidebar/Sidebar.jsx
+++ Ahmat Prayoga Sembiring_123140053_Pertemuan3/src/components/Sidebar/Sidebar.jsx
@@ -11,6 +11,7 @@ export default function Sidebar() {
       <nav className="nav-menu">
         <Link to="/" className={`nav-item ${isActive('/') ? 'active' : ''}`}>
           <img src="/image.png" alt="Buku" className="nav-icon" style={{ width: '80px', height: '80px', objectFit: 'contain' }} />
+          <span className="nav-text">Buku</span>
         </Link>
 
         <Link to="/stats" className={`nav-item ${isActive('/stats') ? 'active' : ''}`}>
